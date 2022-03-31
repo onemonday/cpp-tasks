0a55a9483842a97a7703473074659f03f29d79db
onemonday <karakyrt13534@gmail.com>
2022-03-31 15:22:54 +0500

    [eratosphen] initial commit

---
+++ eratosphen/eratosphen.cpp
@@ -0,0 +1,21 @@
+//
+// Created by karak on 3/31/2022.
+//
+#include <iostream>
+#include "limits"
+
+int main() {
+    unsigned int size;
+    unsigned int* array = new unsigned int[size];
+    unsigned int N;
+
+    std::cout << "Enter N" << std:: endl;
+    std::cin >> N;
+
+    size = N / 32 + 1;
+
+    for (int i = 0; i < size; i++)
+        array[i] = std::numeric_limits<unsigned int>::max();
+
+
+};
